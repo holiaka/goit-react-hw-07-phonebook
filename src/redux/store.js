91aabf6d670816ef3ad67f7a2d30be807bfb7c9d
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2023-03-18 21:37:08 +0200

    Preprocessing

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { contactReducer } from './sliceContacts';
 import { filterReducer } from './sliceFilter';
-// import { contactsReducer, filterReducer } from './reducer';
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactReducer,
     filter: filterReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  },  
 });
 
-export const persistor = persistStore(store);
